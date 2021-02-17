2bdb40cb00e04399a3a5ff2383dd5c1c754558b0
Isabelle <imrivera@loyola.edu>
2021-02-17 01:14:07 -0500

    Created the database but not sure if it fully works or is on localhost

--- Code-ArtProject/database.py
+++ Code-ArtProject/database.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 
+_name_ = "CodeArtDatabase"
+
 app = Flask(_name_)
 app.config["SQLAlchemy_DATABASE_URI"] = "sqlite:////Code-ArtProject/ca_database.db"
 db = SQLAlchemy(app)
 
-class User(db.Model);
+class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80), unique = True, nullable = False)
     email = db.Column(db.String(120), unique = True, nullable = False)
 
-    def_repr_(self):
+    def _repr_(self):
         return "<User %r>" % self.username
 
 
