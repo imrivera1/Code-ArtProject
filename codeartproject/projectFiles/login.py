a4d4d864ff7799c72d04cefec8be8a11700832d6
Isabelle <imrivera@loyola.edu>
2021-04-10 21:03:36 -0400

    added Login method, logout method, and signin html code

--- codeartproject/projectFiles/login.py
+++ codeartproject/projectFiles/login.py
@@ -46,32 +46,32 @@ class AdminModelViewEvent(ModelView):
 def load_user(id):
     return Account.query.get(id)
 
-#@login_blueprint.route('/login', methods=['GET','POST'])
-#@login_blueprint.route('/login.html', methods=['GET','POST'])
-#@login_blueprint.route('/signin.html', methods=['GET','POST'])
-#def login():
-    #form = LoginForm()
+@login_blueprint.route('/login', methods=['GET','POST'])
+@login_blueprint.route('/login.html', methods=['GET','POST'])
+@login_blueprint.route('/signin.html', methods=['GET','POST'])
+def login():
+    form = LoginForm()
 
-    #if form.validate_on_submit():
-        #user = Account.query.filter_by(email=str(form.email.data).lower()).first()
-        #if user:
-            #if check_password_hash(user.password,str(form.password.data)):
-                #login_user(user, remember = form.remember_me.data)
-                #return redirect("/admin")
-            #return "Error: Incorrect Credentials"
-        #return "Admin Account Does Not Exist"
-    #return render_template("signin.html", form=form)
+    if form.validate_on_submit():
+        user = Account.query.filter_by(email=str(form.email.data).lower()).first()
+        if user:
+            if check_password_hash(user.password,str(form.password.data)):
+                login_user(user)
+                return redirect("/admin")
+            return "Error: Incorrect Credentials"
+        return "Admin Account Does Not Exist"
+    return render_template("signin.html", form=form)
 
-#@login_blueprint.route('/logout')
-#@login_blueprint.route('/logout.html')
-#@login_required
-#def logout():
-#    logout_user()
-#    return redirect('/signin.html')
+@login_blueprint.route('/logout')
+@login_blueprint.route('/logout.html')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/signin.html')
 
-#class LoginForm(FlaskForm):
-    #email = StringField('Email', validators=[InputRequired(), Length(min=4,max=64)])
-    #password = PasswordField('Password', validators=[InputRequired(), Length(min=8,max=64)])
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Length(min=4,max=64)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8,max=64)])
     #remember_me = BooleanField('Remember Me')
 
 
