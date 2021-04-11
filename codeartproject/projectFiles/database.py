572cbc88e23ff83133b1a77db073935b0d22cf11
Isabelle <imrivera@loyola.edu>
2021-04-11 01:54:16 -0400

    added static url path to app line in database, added print statement in login

--- codeartproject/projectFiles/database.py
+++ codeartproject/projectFiles/database.py
@@ -11,7 +11,7 @@ import endpoints.internships as internship
 import endpoints.events as event
 
 #init app
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 #bootstrap
 Bootstrap(app)
