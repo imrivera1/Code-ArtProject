cb58349f29884c54749ae4e240663115767ae64d
Isabelle <imrivera@loyola.edu>
2021-04-18 19:24:48 -0400

    added print statements in account create

--- codeartproject/projectFiles/endpoints/account.py
+++ codeartproject/projectFiles/endpoints/account.py
@@ -104,6 +104,7 @@ class AccountInfo(Resource):
 
 
 class AccountCreate(Resource):
+    print("BEFORE")
     parser = reqparse.RequestParser()
     parser.add_argument('is_admin', type=bool)
     parser.add_argument('is_student', type=bool)
@@ -115,6 +116,8 @@ class AccountCreate(Resource):
     parser.add_argument('gender', type=str)
     parser.add_argument('attributes', type=str)
     parser.add_argument('password', type=str)
+
+    print("After parse, before post method")
 
     def post(self):
         print(request.data)
