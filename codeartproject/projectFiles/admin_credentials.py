b668b164e371a7aec2e99c73b22e432ea97c0a6d
Isabelle <imrivera@loyola.edu>
2021-04-18 20:36:50 -0400

    testing id for admin

--- codeartproject/projectFiles/admin_credentials.py
+++ codeartproject/projectFiles/admin_credentials.py
@@ -3,8 +3,12 @@ from database import db
 from werkzeug.security import generate_password_hash
 import uuid
 
-admin=Account(id=str( uuid.uuid4() ), is_admin=True, is_student=False, first_name="Romina", last_name="Polo", email="admin@gmail.com", 
-graduation="", birthday="", gender="female", attributes="", password=generate_password_hash("admin@OnlyPass!", method="SHA512"))
+created_admin_id = uuid.uuid4()
 
-db.session.add(admin)
-db.session.commit()
+print( str(created_admin_id) )
+
+#admin=Account(id=str( created_admin_id ), is_admin=True, is_student=False, first_name="Romina", last_name="Polo", email="admin@gmail.com", 
+#graduation="", birthday="", gender="female", attributes="", password=generate_password_hash("admin@OnlyPass!", method="SHA512"))
+
+#db.session.add(admin)
+#db.session.commit()
