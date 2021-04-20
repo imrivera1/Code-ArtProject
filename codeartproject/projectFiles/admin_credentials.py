c219319d8a2a0ad095b8d59257ad390b5126cae2
Isabelle <imrivera@loyola.edu>
2021-04-19 20:36:03 -0400

    checking admin id

--- codeartproject/projectFiles/admin_credentials.py
+++ codeartproject/projectFiles/admin_credentials.py
@@ -3,9 +3,11 @@ from database import db
 from werkzeug.security import generate_password_hash
 import uuid
 
-created_admin_id = uuid.uuid4()
+created_admin_id = int( uuid.uuid4() )
 
-admin=Account(id=int(created_admin_id ), is_admin=True, is_student=False, first_name="Romina", last_name="Polo", email="admin@gmail.com", 
+print(created_admin_id)
+
+admin=Account(id=created_admin_id, is_admin=True, is_student=False, first_name="Romina", last_name="Polo", email="admin@gmail.com", 
 graduation="", birthday="", gender="female", attributes="", password=generate_password_hash("admin@OnlyPass!", method="SHA512"))
 
 db.session.add(admin)
