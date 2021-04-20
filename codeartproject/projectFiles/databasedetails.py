ad3356384fbc69cee4d9fc9d0edb6df87469b3be
imrivera1 <imrivera@loyola.edu>
2021-04-20 19:58:28 -0400

    returned primary key and changed text to string

--- codeartproject/projectFiles/databasedetails.py
+++ codeartproject/projectFiles/databasedetails.py
@@ -19,7 +19,7 @@ class Account(db.Model, UserMixin):
     gender: str
     attributes: str
     """
-    id = db.Column(db.Text, unique = True, autoincrement=False, nullable = False)
+    id = db.Column(db.String, primary_key = True, unique = True, nullable = False)
     is_admin = db.Column(db.Boolean, nullable = False)
     is_student = db.Column(db.Boolean, nullable = False)
     first_name = db.Column(db.Text, nullable = False)
@@ -42,7 +42,7 @@ class Internship(db.Model):
     link: str
     details: str
     """
-    id = db.Column(db.Text, unique = True, nullable = False)
+    id = db.Column(db.String, primary_key = True, unique = True, nullable = False)
     location = db.Column(db.Text, nullable = False)
     company = db.Column(db.Text, nullable = False)
     role = db.Column(db.Text, nullable = False)
@@ -62,7 +62,7 @@ class Event(db.Model):
     details: str
     """
 
-    id = db.Column(db.Text, unique = True, nullable = False)
+    id = db.Column(db.String, primary_key = True, unique = True, nullable = False)
     event_name = db.Column(db.Text, nullable = False)
     organizers = db.Column(db.Text, nullable = False)
     location = db.Column(db.Text, nullable = False)
