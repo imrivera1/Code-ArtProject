330728ed37de661cd9f09535e38df8db09b04269
imrivera1 <imrivera@loyola.edu>
2021-04-20 20:40:07 -0400

    fixed admin creds

--- codeartproject/projectFiles/admin_credentials.py
+++ codeartproject/projectFiles/admin_credentials.py
@@ -3,9 +3,11 @@ from database import db
 from werkzeug.security import generate_password_hash
 import uuid
 
-created_admin_id = uuid.uuid4()
+string_uuid = str( uuid.uuid4() )
+half_len_of_string = ( len(string_uuid) )/2
+created_admin_id = int( string_uuid[:half_len_of_string] )
 
-admin=Account(id=str( created_admin_id ), is_admin=True, is_student=False, first_name="Romina", last_name="Polo", email="admin@gmail.com", 
+admin=Account(id=created_admin_id, is_admin=True, is_student=False, first_name="Romina", last_name="Polo", email="admin@gmail.com", 
 graduation="", birthday="", gender="female", attributes="", password=generate_password_hash("admin@OnlyPass!", method="SHA512"))
 
 db.session.add(admin)
