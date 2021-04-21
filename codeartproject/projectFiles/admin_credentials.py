a3e2de2d56955caf56a3f446199f2aaac0641538
imrivera1 <imrivera@loyola.edu>
2021-04-20 20:42:54 -0400

    removed some paranthesis

--- codeartproject/projectFiles/admin_credentials.py
+++ codeartproject/projectFiles/admin_credentials.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash
 import uuid
 
 string_uuid = str( uuid.uuid4() )
-half_len_of_string = ( len(string_uuid) )/2
+half_len_of_string = len(string_uuid)/2
 created_admin_id = int( string_uuid[:half_len_of_string] )
 
 admin=Account(id=created_admin_id, is_admin=True, is_student=False, first_name="Romina", last_name="Polo", email="admin@gmail.com", 
