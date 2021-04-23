500941dda01df7016c2460cf26bb974a37f82740
imrivera1 <imrivera@loyola.edu>
2021-04-23 13:31:27 -0400

    did same for initializating database

--- codeartproject/projectFiles/initialization/init_db.py
+++ codeartproject/projectFiles/initialization/init_db.py
@@ -1,2 +1,7 @@
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
 from database import db
 db.create_all()
