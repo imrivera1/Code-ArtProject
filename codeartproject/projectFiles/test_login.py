172dea05211919147570875adda2fc4de1b6f154
imrivera1 <imrivera@loyola.edu>
2021-04-22 21:23:20 -0400

    changed birthday format for test login

--- codeartproject/projectFiles/test_login.py
+++ codeartproject/projectFiles/test_login.py
@@ -8,7 +8,7 @@ half_len_of_string = int( len(string_uuid)/2 )
 created_id = int( string_uuid[:half_len_of_string] )
 
 student = Account(id=created_id, is_admin=False, is_student=True, first_name="Alexa", last_name="Fern", 
-email="student@gmail.com", graduation="", birthday="04/11/1993", age=27, gender="female", attributes="", password=generate_password_hash("password",method="SHA512"))
+email="student@gmail.com", graduation="", birthday="04/11/93", age=27, gender="female", attributes="", password=generate_password_hash("password",method="SHA512"))
 
 db.session.add(student)
 db.session.commit()
