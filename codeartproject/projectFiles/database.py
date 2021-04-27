4864da6e6f82bf5eb5f918db6b318de0ed5103be
imrivera1 <imrivera@loyola.edu>
2021-04-27 02:03:54 -0400

    changed admin template

--- codeartproject/projectFiles/database.py
+++ codeartproject/projectFiles/database.py
@@ -36,7 +36,7 @@ login_manager.init_app(app)
 app.register_blueprint(login_blueprint)
 
 #admin dashboard
-admin = Admin(app, name = "Admin", url = "/admin", endpoint = "admin", template_mode="bootstrap3")
+admin = Admin(app, name = "Admin", url = "/admin", endpoint = "admin", template_mode="bootstrap.min.css")
 admin.add_view(AdminModelViewAcc(Account, db.session))
 admin.add_view(AdminModelViewIntern(Internship, db.session))
 admin.add_view(AdminModelViewEvent(Event, db.session))
